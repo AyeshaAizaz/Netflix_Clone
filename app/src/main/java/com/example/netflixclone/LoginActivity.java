09d77915a9cde483b9f075b07fa807bd473b46bb
PMYLS <ayeshaaizaz2@gmail.com>
2024-01-07 01:06:34 +0500

    activity_sign_up.xml, SignUpActivity.java, fragment_sign_up.xml, LoginActivity.java, SignUpFragment.java, strings.xml, colors.xml, baseline_close_24.xml,
    Login Email Validation Check

--- app/src/main/java/com/example/netflixclone/LoginActivity.java
+++ app/src/main/java/com/example/netflixclone/LoginActivity.java
@@ -4,33 +4,80 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button login, signUp;
+    Button login, signUp, forgot;
+    ImageButton im;
+    private static final String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final String phonePattern = "^\\+\\d{10,}$\n";
+    private static final Pattern emailpattern = Pattern.compile(emailPattern);
+    private static final Pattern phonepattern = Pattern.compile(phonePattern);
+    EditText email, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.signIn);
         signUp = (Button) findViewById(R.id.signUp);
+        im = (ImageButton) findViewById(R.id.im);
+        forgot = (Button) findViewById(R.id.forgot);
         login.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+        im.setOnClickListener(this);
+        forgot.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
 
         if(v.getId() == R.id.signIn){
-            Intent login_intent = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(login_intent);
+            if(validation_checks()){
+                Intent login_intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(login_intent);
+            }
+            else{
+                Toast.makeText(LoginActivity.this, "Incorrect Email/Phone", Toast.LENGTH_SHORT).show();
+            }
         }
         else if(v.getId() == R.id.signUp){
             Intent signUp_intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(signUp_intent);
         }
+        else if(v.getId() == R.id.im){
+            Intent back_intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(back_intent);
+        }
+        else if(v.getId() == R.id.forgot){
+            //make activity
+        }
 
+    }
+
+    private boolean validation_checks(){
+        boolean valid = false;
+        if(email.getText().toString() != null && pass.getText().toString() != null){
+            Matcher emailmatcher = emailpattern.matcher(email.getText().toString());
+            Matcher phonematcher = phonepattern.matcher(email.getText().toString());
+            if(emailmatcher.matches() || phonematcher.matches()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/netflixclone/SignUpActivity.java
+++ app/src/main/java/com/example/netflixclone/SignUpActivity.java
@@ -1,14 +1,45 @@
 package com.example.netflixclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    Button start;
+    ImageButton im1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        start = (Button) findViewById(R.id.start);
+        im1 = (ImageButton) findViewById(R.id.im1);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new SignUpFragment());
+            }
+        });
+
+        im1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cross_intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(cross_intent);
+            }
+        });
+    }
+
+    private void replaceFragment(SignUpFragment signUpFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.layout, signUpFragment);
+        fragmentTransaction.commit();
     }
 }
